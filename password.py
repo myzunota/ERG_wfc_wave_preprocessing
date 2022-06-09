93bbe8a604c8c991fa3113660e0716604e24cd7a
myzunota <107171396+myzunota@users.noreply.github.com>
2022-06-09 18:08:45 +0900

    Update password.py

--- password.py
+++ password.py
@@ -6,6 +6,6 @@ Created on Thu Apr 28 12:56:38 2022
 @author: mizunoyuta
 """
 
-base_url = 'https://ergsc.isee.nagoya-u.ac.jp/data/ergsc/satellite/erg/'
-user = 'erg_project'
-password = 'geospace'
+base_url = 'https://....../data/ergsc/satellite/erg/'
+user = 'sample'
+password = 'sample'
